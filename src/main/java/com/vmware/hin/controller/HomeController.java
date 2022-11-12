0946805aac6100021bff32017e75042d50072657
Hin Lam <3368889+hinyinlam@users.noreply.github.com>
2022-11-12 15:49:01 +0800

    update catalog and controller links

--- src/main/java/com/vmware/hin/controller/HomeController.java
+++ src/main/java/com/vmware/hin/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String getIndexPage(){
-        return "<img src='/vmware-explore-sg.png'><br/><img src='/nyan-cat.gif'/>";
+        return "<img src='/vmware-explore-sg.png'><br/><img src='/nyan-cat.gif'/><br/><a href='/v3/api-docs>OpenAPI v3 for this app</a>";
     }
 
 }
