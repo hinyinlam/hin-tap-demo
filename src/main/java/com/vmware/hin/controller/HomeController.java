a7ca5dd5ebf9bf04e37ab0c2bbd06138796b75c6
Hin Lam <3368889+hinyinlam@users.noreply.github.com>
2023-06-19 11:16:05 +1000

    Update site doc and app background

--- src/main/java/com/vmware/hin/controller/HomeController.java
+++ src/main/java/com/vmware/hin/controller/HomeController.java
@@ -3,10 +3,14 @@ package com.vmware.hin.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class HomeController {
+    Logger log = Logger.getLogger(this.getClass().getName());
     @GetMapping("/")
     public String getIndexPage(){
+        log.info("Testing from Hin");
         return "<img src='/vmware-explore-sg.png'><br/><br/><a href='/v3/api-docs'>OpenAPI v3 for this app</a>";
     }
 
